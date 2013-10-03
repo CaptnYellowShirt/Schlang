c3d49976728817871212d00ca67af7d585e59edc
BJ Dillon <CaptnYellowShirt@gmail.com>
2013-10-02 21:07:18 -0400

    rohrpost test

--- rohrpost/rohrpost.c
+++ rohrpost/rohrpost.c
@@ -1,83 +1,217 @@
 
-
-#include <pthread.h>
+#define _GNU_SOURCE 1
+#include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include "rohrpost.h"
 
 
-
-/*  laytube_toFile - Lay a new tube to a file
+/*  laytube_toFile - Lay a new penumatic tube to a file
 
         #include "rohrpost.h"
 
-        tubeID laytube_toFile(comedi_t *dev, int subdevice, int fileno)
+        int laytube_toFile(tubeID *pTube)
 
     Description
 
-        A fast and efficient method to asyncronously stream raw data from a COMEDI subdev ring buffer to disk.
-        Function should create a child process (e.g. pneumatic tube) that is responsible for transfering data
-        from the subdevice's ring buffer to the file described by fileno. Upon sucessful execution of the child
-        process, function will return a valid tubeID number.
-        The child process will automatically listen for data updates to the ring buffer. Once an update is detected,
-        the child process will append data to the file described by fileno -- increasing the size of the file as
-        data is appended.
-        Child process will run until closetube() is called with the tubeID number.
+        A fast and efficient method to asyncronously stream raw data
+        from a COMEDI subdev ring buffer to disk. Function creates a
+        child thread (e.g. pneumatic tube) that is responsible for
+        transfering data from the subdevice's ring buffer (described
+        by pTube.mouth) to a disk file (described by pTube.tail).
+
+        The child thread will automatically listen for data updates
+        to the ring buffer. Once an update is detected, the child
+        thread will append data to the file described by pTube.tail
+        -- increasing the size of the file as data is appended.
+
+        File described by pTube.tail must be opened in a binary and
+        append mode. File may be preallocated, however laytube_toFile
+        will overwrite anything that is present in the file upon
+        execution.
+
+        Child thread will run until pTube.tubeCmd is set with the
+        TUBE_STOP flag. Once this flag is detected, the child will
+        stop its data transfer, perform a final update to the
+        TubeID structure, truncate the pTube.tail file to a size
+        which corresponds to the last data byte that was moved,
+        and close the thread.
+
+        Upon execution laytube_toFile will update the TubeID structure
+        and return a integer describing its execution sucess.
 
     Arguments
 
-        *dev        - Pointer to COMEDI device that is currently open
-        sudevice    - Interger describing the subdevice
-        fileno      - File descriptor of open, empty file
+        *pTube   - Pointer to an instance of a TubeID strucutre
 
     Returns
 
-        tubeID num  - Sucess!
-        -1          - Bad device
-        -2          - Bad file
+         0          - Sucess!
+         -1         - Bad COMEDI File Descriptor
+         -2         - Bad destiation File Descriptor
+
 */
 
 
-tubeID laytube_toFile(comedi_t *dev, int fileno){
+int laytube_toFile(tubeID *pTube){
 
-    const char *board_name;
-    int file_flags;
-    pthread_t digger;
-    int files[2];
+    int file_flags[2];
 
 
+    /* Check COMEDI file descriptor */
+    pTube->mouth = comedi_fileno(pTube->dev);
 
-    // Check COMEDI device and subdevice
-    board_name = comedi_get_board_name(dev);
+    file_flags[0] = fcntl(pTube->mouth, F_GETFD);
 
-    if (board_name == NULL){
+    // Is fd open? ...
+    if (file_flags[0] == EBADF){
         return(-1);
     }
 
 
-    // Check fileno
-    file_flags = fcntl(fileno, F_GETFD);
+    // Check destition file descriptor
+    pTube->tail = fileno(pTube->dest);
 
-    if (file_flags == -1){
+    file_flags[1] = fcntl(pTube->tail, F_GETFD);
+
+    // Is fd open? ...
+    if (file_flags[1] == EBADF){
         return(-2);
     }
 
-    files[0] = comedi_fileno(dev);
-    files[1] = fileno;
+    /* Create new thread instance of data handeling process */
+    pthread_create(&(pTube->threadNo), NULL, _laytube_toFile, (void *)pTube );
 
-    pthread_create(&digger, NULL, _laytube, (void *)files );
+    /* Block laytube_toFile from returning until new pTube is setup */
+    while ( !(pTube->tubeStatus & TUBE_INPLACE)){
+        usleep(100);  /* 1/10 sec */
+    }
 
-    return((tubeID) digger);
+    return(0);
 
 }
 
 
-void *_laytube(void *args){
+void *_laytube_toFile(void *args){
 
-    int mouth_fd = ((int *)args)[0];
-    int tail_fd = ((int *)args)[1];
+    int i;
+    int subdev_flags;
+    int overhead;
+    tubeID *pTube;
+    rohrStation sendingStation;
+    rohrStation receivingStation;
 
-    printf("\n\nmouth_fd: %i\ntail_fd: %i\n\n", mouth_fd, tail_fd);
+    pTube = (tubeID *) args;
 
-    pthread_exit(NULL);
+    void *dest;
+    int offset;
 
+
+
+    /* Populate Station Information */
+    sendingStation.stationSize = comedi_get_buffer_size(pTube->dev, pTube->subdev);
+    sendingStation.fd = pTube->mouth;
+    receivingStation.stationSize = _file_size(pTube->tail);
+    receivingStation.fd = pTube->tail;
+
+    /* Set size of samples */
+    subdev_flags = comedi_get_subdevice_flags(pTube->dev, pTube->subdev);
+    if (subdev_flags | SDF_LSAMPL){
+        sendingStation.packageSize = sizeof(lsampl_t);
+    }else{
+        sendingStation.packageSize = sizeof(sampl_t);
+    }
+    receivingStation.packageSize = sendingStation.packageSize;
+
+
+    /* Set up memory maps */
+    sendingStation.address = mmap(NULL, sendingStation.stationSize, PROT_READ, MAP_PRIVATE, pTube->mouth, 0);
+    if (sendingStation.address == MAP_FAILED){
+        pthread_exit(NULL);
+    }
+
+    receivingStation.address = mmap(NULL, receivingStation.stationSize, PROT_WRITE, MAP_SHARED | MAP_HUGETLB, pTube->tail, 0);
+    if (receivingStation.address == MAP_FAILED){
+        pthread_exit(NULL);
+    }
+
+
+    /* Stations are setup... set status and command flags... */
+    sendingStation.lastSent = 0;
+    sendingStation.tobeSent = 0;
+    pTube->tubeCmd = 0x00;
+    pTube->tubeStatus = TUBE_INPLACE; /* <-- un-blocks laytube_toFile to return to calling function */
+
+
+
+    while( !(pTube->tubeCmd & TUBE_STOP )){
+
+        sendingStation.tobeSent += comedi_get_buffer_contents(pTube->dev, pTube->subdev); /* bytes */
+
+        /* If toBeSent is the same number as lastSent then no data is ready for pick up ... rest */
+        if (sendingStation.tobeSent == sendingStation.lastSent){
+            usleep(10); /* TODO: optimize by setting sleep number to best guess at when data might show up again */
+            /* do other stuff like update pTube status */
+        }
+        else /* Assume fresh data is in ring buffer... */
+        {
+            overhead = receivingStation.stationSize - sendingStation.lastSent + sendingStation.tobeSent;
+            if (overhead <= 0){
+                if (_growStation(&receivingStation, 65536)){
+                    /* Error checking */
+                }
+            }
+
+            dest = receivingStation.address + sendingStation.lastSent;
+
+            /* Mem Copy ... */
+            for (i = 1; i > sendingStation.tobeSent; i++ ){
+                offset = (sendingStation.lastSent + i) % sendingStation.packageSize;
+                *(char *)(dest + i) = *(char *)(sendingStation.address + offset );
+            }
+
+            /* Update station status */
+
+        }
+
+
+    }
+
+    /* clean up Stations, flush tube, truncate file */
+    pthread_exit(0);
+
+
+}
+
+
+long unsigned _file_size(int fd){
+    struct stat buf;
+
+    fstat(fd, &buf);
+
+    return buf.st_size;
+}
+
+
+int _growStation(rohrStation *smallStation, int expansion){
+
+    int oldSz = smallStation->stationSize;
+    int newSz = oldSz + expansion;
+
+
+    /* Expand underlying file ... */
+    if (ftruncate(smallStation->fd, expansion) ){
+        return -1;
+    }
+
+    /* ... and memory map */
+    if ((smallStation->address = mremap(smallStation->address, oldSz, newSz, MREMAP_MAYMOVE) )) {
+        return -1;
+    }
+
+    smallStation->stationSize = newSz;
+
+    return 0;
 }
--- rohrpost/rohrpost.h
+++ rohrpost/rohrpost.h
@@ -1,8 +1,65 @@
 
+
+#include <stdio.h>
+#include <pthread.h>
 #include <comedilib.h>
 
-typedef long int tubeID;
+
+/* tubeStatus Flags */
+#define TUBE_INPLACE    0x01    /* Tube has been created */
+#define TUBE_ACTIVE     0x02    /* Tube has passed data */
+#define TUBE_WAIT       0x04    /* Tube is waiting for data to appear at mouth or has been paused */
+
+/* tubeCmd Flags */
+#define TUBE_PAUSE      0x01    /* Temporarly stop data transfer */
+#define TUBE_WARP       0X02    /* Increase transfer speed. e.g. stop updatting state */
+#define TUBE_STOP       0x04    /* Stop data transfer, clean up tube, and remove it */
 
 
-void *_laytube(void *args);
-tubeID laytube_toFile(comedi_t *dev, int fileno);
+
+typedef struct _tubeID{
+
+    /* User must supply the following three items: */
+
+    comedi_t *dev;  /* Pointer to an open COMEDI device structure */
+    int subdev;     /* COMEDI device subdevice number */
+    FILE *dest;     /* Pointer to an open file */
+
+
+    /* Code will fill-in the following items: */
+
+    int mouth; /* File Number of location of data to be moved */
+    int tail; /* File Number of the location to which the data is moved */
+
+    pthread_t threadNo; /* Thread Number of thread preforming the data copying */
+
+    double bytesMoved; /* Best guess at number of bytes that have been pushed though the tube */
+
+    unsigned int tubeStatus; /* Bitwise tube status flags */
+    unsigned int tubeCmd;    /* Bitwise tube command flags */
+
+
+
+} tubeID;
+
+typedef struct _rohrStation{
+    int fd; /* File Descriptor of underlying File */
+
+    long unsigned stationSize; /* Number of bytes of COMEDI buffer or file size */
+
+    void *address; /* memory map address */
+
+    long unsigned tobeSent;
+    long unsigned lastSent;
+
+    unsigned int packageSize;
+
+} rohrStation;
+
+
+
+
+int laytube_toFile(tubeID *Tube);
+void *_laytube_toFile(void *args);
+long unsigned _file_size(int fd);
+int _growStation(rohrStation *smallStation, int expansion);
